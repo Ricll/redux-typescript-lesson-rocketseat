8b3d91c886db200d6e7aceb3fe3bd558fce46902
Ricll <ricllima@hotmail.com>
2020-11-05 17:43:18 -0300

    Add FailedStockCheck

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,41 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
+import { IProduct } from '../store/modules/cart/types';
+
+// import { Container } from './styles';
+
+interface CatologItemProps {
+  product: IProduct;
+}
+
+const CatalogItem: React.FC<CatologItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const hasFailedStockCheck = useSelector<IState, boolean>((state) => {
+    return state.cart.failedStockCheck.includes(product.id);
+  });
+
+  const hanldeAddProductToCart = useCallback(() => {
+    dispatch(addProductToCartRequest(product));
+  }, [dispatch, product]);
+
+  return (
+    <article>
+      <strong>{product.title}</strong>
+      {' - '}
+      <span>{product.price}</span>
+
+      {'  '}
+      <button type="button" onClick={hanldeAddProductToCart}>
+        Comprar
+      </button>
+      {hasFailedStockCheck && (
+        <span style={{ color: 'red' }}>Falta de estoque</span>
+      )}
+    </article>
+  );
+};
+
+export default CatalogItem;
